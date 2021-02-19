4b08fd8954c1d2bb596891703a8485bce092ab93
Your Name <zack.cammack@gmail.com>
2021-02-19 04:20:36 -0600

    Think I've figured out how to organize  this code better. I'll start writing functions for these two classes I've created. Also made a constant with form information for later.

---
+++ frontend/src/cookbook.js
@@ -0,0 +1,7 @@
+const cookbookForm = `
+    <label><strong>Name: </strong></label><br/>
+    <input type="text" id="name"><br/>
+    <input type="hidden" id="cookbookID">
+    `
+
+class Cookbook
---
+++ frontend/src/recipe.js
@@ -0,0 +1,13 @@
+const recipeForm = `
+<label><strong>Name: </strong></label><br/>
+<input type="text" id="name"><br/>
+<input type="hidden" id="recipeID">
+<label><strong>Ingredients: </strong></label><br/>
+<input type="text" id="ingredients"><br/>
+<label><strong>Directions: </strong></label><br/>
+<input type="text" id="directions"><br/>
+<label><strong>Authored By: </strong></label><br/>
+<input type="text" id="author"><br/>
+`
+
+class Recipe
